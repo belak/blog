0a6dec14f695270822e90a510fab60d1fbeef5a4
Kaleb Elwert <belak@coded.io>
2026-03-25 15:47:04 -0700

    Fix exported nix package

--- flake.nix
+++ flake.nix
@@ -90,14 +90,14 @@
           };
 
           packages = {
-            default = pkgs.myBuildGoModule rec {
+            default = pkgs.buildGoModule rec {
               pname = "belak-blog";
               version = "0.0.1";
               src = ./.;
               subPackages = [
                 "cmd/belak-blog"
               ];
-              vendorHash = "";
+              vendorHash = "sha256-q95d5DmXFDELfkg3/oL0tF0qZDrXs0JjIZXTNA3fWtY=";
             };
           }
           // (lib.packagesFromDirectoryRecursive {
